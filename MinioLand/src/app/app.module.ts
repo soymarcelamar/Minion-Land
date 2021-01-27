cd2f9e2042fcc8b27c410c87615267c32308aed4
DayanaM <dmmartinez269@misena.edu.co>
2021-01-27 17:45:01 -0500

    Angular components and router SPA

--- MinioLand/src/app/app.module.ts
+++ MinioLand/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app.routes';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/shared/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { MinionsComponent } from './components/minions/minions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    MinionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
